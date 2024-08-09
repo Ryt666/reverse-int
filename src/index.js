97c9c56bd164cd41127ae81abaa9069a8c5faa77
palina.patapava <palina.patapava@azati.com>
2024-08-09 15:17:21 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-module.exports = function reverse (n) {
-  let r=0;
-  while(n) {
-    r = r *10 + n%10;
-    n= Math.floor(n / 10)
-  }
+module.exports = function reverse(n) {
+    let r = 0;
+    while (n) {
+        r = r * 10 + n % 10;
+        n = Math.floor(n / 10)
+    }
+    return r
 }
+
